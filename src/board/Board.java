771f1a5df450233c543c82ae41ff01f1179a54a0
Peter Liley <peterandrewliley@gmail.com>
2021-07-24 21:11:23 +1200

    Implement board and squares

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,20 +1,179 @@
 package board;
 
+import board.Square.Room;
 import game.*;
 
 public class Board {
-	
-	public static enum Direction{UP, DOWN, LEFT, RIGHT}
-	
+
+	public static enum Direction {
+		UP, DOWN, LEFT, RIGHT
+	}
+
 	private Square[][] squares;
-	
+
 	public Board() {
 		squares = new Square[24][24];
 		makeBoard();
 	}
-	
+
+	/**
+	 * Builds the board by setting the 'walls' of all squares
+	 * according to board layout
+	 */
 	private void makeBoard() {
-		//TODO: implement walls and rooms
+
+		// this string represents board layout. The key below shows which characters
+		// represent what state of the tile's walls.
+		// u = wall up
+		// l = wall left
+		// r = wall right
+		// d = wall down
+		// q = wall up & left
+		// p = wall up & right
+		// z = wall down and left
+		// m = wall down and right
+		// (space) = no walls
+		// NB this is NOT meant for visual utility or to be seen by the player - only a simple way to build the board
+
+		String s = "";
+		s += "quuuuuuuuuuuuuuuuuuuuuup";
+		s += "l ddddd          ddddd r";
+		s += "lrquuupl        rquuuplr";
+		s += "lrl             rl   rlr";
+		s += "lrl   rl   dd   rl   rlr";
+		s += "lrl   rl  rqpl       rlr";
+		s += "lrzdddml  rzml  rzdddmlr";
+		s += "l uuuuu    uu    uuuuu r";
+		s += "l                       ";
+		s += "l        ddd dd        r";
+		s += "l    dd rquu upl dd    r";
+		s += "l   rqplrl      rqpl   r";
+		s += "l   rzml      rlrzml   r";
+		s += "l    uu rzd ddml uu    r";
+		s += "l        uu uuu        r";
+		s += "l                      r";
+		s += "l d ddd    dd    d ddd r";
+		s += "lrq uupl  rqpl  rq uuplr";
+		s += "lrl       rzml  rl   rlr";
+		s += "lrl   rl   uu   rl   rlr";
+		s += "lrl   rl             rlr";
+		s += "lrzdddml        rzdddmlr";
+		s += "l uuuuu          uuuuu r";
+		s += "zddddddddddddddddddddddm";
+
+		
+		// and this string layout is converted into actual walls inside square objects
+		for(int x = 0; x < 24; x++) {
+			for(int y = 0; y < 24; y++){
+				char curr = s.charAt(y*24 + x);
+				if(curr == 'u') squares[x][y].setUp(false);
+				else if(curr == 'l') squares[x][y].setLeft(false);
+				else if(curr == 'd') squares[x][y].setDown(false);
+				else if(curr == 'r') squares[x][y].setRight(false);
+				else if(curr == 'q') {
+					squares[x][y].setUp(false);
+					squares[x][y].setLeft(false);
+				}
+				else if(curr == 'p') {
+					squares[x][y].setUp(false);
+					squares[x][y].setRight(false);
+				}
+				else if(curr == 'z') {
+					squares[x][y].setDown(false);
+					squares[x][y].setLeft(false);
+				}
+				else if(curr == 'm') {
+					squares[x][y].setDown(false);
+					squares[x][y].setRight(false);
+				}
+				
+				if((x > 2 && x < 6) && (y > 2 && y < 6)) {
+					squares[x][y].setIsRoom();
+					squares[x][y].setRoom(Room.HAUNTED_HOUSE);
+				}
+				else if((x > 17 && x < 21) && (y > 2 && y < 6)) {
+					squares[x][y].setIsRoom();
+					squares[x][y].setRoom(Room.MANIC_MANOR);
+				}
+				else if((x > 2 && x < 6) && (y > 17 && y < 21)) {
+					squares[x][y].setIsRoom();
+					squares[x][y].setRoom(Room.CALAMITY_CASTLE);
+				}
+				else if((x > 17 && x < 21) && (y > 17 && y < 21)) {
+					squares[x][y].setIsRoom();
+					squares[x][y].setRoom(Room.PERIL_PALACE);
+				}
+				else if((x > 9 && x < 14) && (y > 10 && y < 13)) {
+					squares[x][y].setIsRoom();
+					squares[x][y].setRoom(Room.VILLA_CELIA);
+				}
+				
+			}
+		}
+		
+		
+		
+	}
+	
+	/**
+	 * Move the given player one square in the given direction
+	 * (by setting the current square's player field to null,
+	 * and the new square's field to the given player)
+	 * @param player
+	 * @param direction
+	 * @return False if move is impossible, else true
+	 */
+	public boolean move(Player player, Board.Direction direction) {
+		
+		int x = player.getLocX();
+		int y = player.getLocY();
+		if(!canMove(player, direction)) return false;
+		
+		if(direction == Direction.DOWN) {
+			squares[x][y].setPlayer(null);
+			squares[x][y+1].setPlayer(player);
+		}
+		else if(direction == Direction.UP) {
+			squares[x][y].setPlayer(null);
+			squares[x][y-1].setPlayer(player);
+		}
+		else if(direction == Direction.LEFT) {
+			squares[x][y].setPlayer(null);
+			squares[x-1][y].setPlayer(player);
+		}
+		else if(direction == Direction.RIGHT) {
+			squares[x][y].setPlayer(null);
+			squares[x+1][y].setPlayer(player);
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Returns true if the given player can move in the given direction
+	 * @param player
+	 * @param direction
+	 * @return
+	 */
+	private boolean canMove(Player player, Board.Direction direction) {
+		Square curr = squares[player.getLocX()][player.getLocY()];
+		if(direction == Direction.DOWN) {
+			if(player.getLocY()+1 > 23) return false;
+			return curr.getDown();
+		}
+		else if(direction == Direction.UP) {
+			if(player.getLocY()-1 < 0) return false;
+			return curr.getUp();
+		}
+		else if(direction == Direction.LEFT) {
+			if(player.getLocX()-1 < 0) return false;
+			return curr.getLeft();
+		}
+		else if(direction == Direction.RIGHT) {
+			if(player.getLocX()+1 < 23) return false;
+			return curr.getRight();
+		}
+		else return false;
 	}
 
 }
--- src/board/Square.java
+++ src/board/Square.java
@@ -11,13 +11,29 @@ public class Square {
 	private Player player;
 	
 	//Accessibility fields - can you move this direction from this square?
-	private boolean up;
-	private boolean down;
-	private boolean left;
-	private boolean right;
+	private boolean up = true;
+	private boolean down = true;
+	private boolean left = true;
+	private boolean right = true;
 	
-	public Square() {}
+	public Square() {
+		
+	}
 	
+	public void setUp(boolean val) { up = val; }
+	public void setDown(boolean val) { down = val; }
+	public void setLeft(boolean val) { left = val; }
+	public void setRight(boolean val) { right = val; }
+	public boolean getUp() { return up; }
+	public boolean getDown() { return down; }
+	public boolean getLeft() { return left; }
+	public boolean getRight() { return right; }
+	public void setPlayer(Player p) { player = p; }
+	public Player getPlayer() { return player; }
+	public void setIsRoom() { isRoom = true; }
+	public boolean getIsRoom() { return isRoom; }
+	public void setRoom(Room room) { this.room = room; }
+	public Room getRoom() { return room; }
 	
 
 }
